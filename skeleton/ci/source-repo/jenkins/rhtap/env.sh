a3badc6c7d1581ba03ac913d48b8bf06a2de4e8c
gitops-update <rhtap@noreplay.com>
2024-09-26 22:25:21 -0400

    gitlab and github

--- skeleton/ci/source-repo/jenkins/rhtap/env.sh
+++ skeleton/ci/source-repo/jenkins/rhtap/env.sh
@@ -2,9 +2,11 @@
 export REBUILD=true
 export SKIP_CHECKS=true
 
+CI_TYPE=${CI_TYPE:-jenkins}  
+
 # from buildah-rhtap
 TAG=$(git rev-parse HEAD)
-export IMAGE_URL=${{ values.image }}:jenkins-$TAG
+export IMAGE_URL=${{ values.image }}:$CI_TYPE-$TAG
 export IMAGE=$IMAGE_URL
 export RESULT_PATH=$DIR/results/temp/files/sbom-url
 
@@ -15,7 +17,7 @@ export BUILD_ARGS=""
 export BUILD_ARGS_FILE=""
 
 # from ACS_*.*
-export DISABLE_ACS=false
+export DISABLE_ACS=true
 # Optionally set ROX_CENTRAL_ENDPOINT here instead of configuring a Jenkins secret
 # export ROX_CENTRAL_ENDPOINT=central-acs.apps.user.cluster.domain.com:443
 export INSECURE_SKIP_TLS_VERIFY=true
@@ -33,10 +35,10 @@ export TARGET_BRANCH=""
 # enterprise contract
 export POLICY_CONFIGURATION="github.com/enterprise-contract/config//rhtap-jenkins"
 #internal, assumes jenkins is local openshift
+export REKOR_HOST=http://rekor-server.rhtap-tas.svc
 export IGNORE_REKOR=false
 export INFO=true
 export STRICT=true
 export EFFECTIVE_TIME=now
 export HOMEDIR=$(pwd)
-export REKOR_HOST=https://rekor-server-rhtap-tas.apps.rosa.rc442-mnk7z-762.zfjd.p3.openshiftapps.com
-export TUF_MIRROR=https://tuf-rhtap-tas.apps.rosa.rc442-mnk7z-762.zfjd.p3.openshiftapps.com
+export TUF_MIRROR=http://tuf.rhtap-tas.svc
